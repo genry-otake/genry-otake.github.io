8b0cc70efd98d6ea86ef393482f94dcdfabd4d73
arkhipenka.v <arkhipenka.v@soft2bet.com>
2023-02-21 15:00:12 +0200

    tw test

--- payment.js
+++ payment.js
@@ -1,3 +1,4 @@
+alert(document.referrer);
 setTimeout(() => {
     window.document.location = './callback.html'
 }, 2000);
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let depButton = document.querySelector('.dep-btn');
 depButton.addEventListener('click', function (){
-    // const paymentWindow = window.open(
-    //     './preloader.html?token=qweqweqweqwe&payment=visa&count=100',
-    //     'qwe',
-    //     'height=500,width=500,left=500,top=200'
-    // );
-    window.open('placeholder.com', '_blank');
-    // setTimeout(()=>{
-    //     paymentWindow.document.location = './payment.html'
-    // },2000)
+    const paymentWindow = window.open(
+        './preloader.html?token=qweqweqweqwe&payment=visa&count=100',
+        'qwe',
+        'height=500,width=500,left=500,top=200'
+    );
+    alert(document.referrer);
+    setTimeout(()=>{
+        paymentWindow.document.location = './payment.html'
+    },2000)
 });
